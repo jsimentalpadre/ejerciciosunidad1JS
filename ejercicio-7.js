86128de9054e562a360701474eb645c110b9f273
jsimentalpadre <107702923+jsimentalpadre@users.noreply.github.com>
2022-06-26 09:34:38 -0600

    Update ejercicio-7.js
    Revisar cambios , para este ejercicio, con respecto al PLUS

--- ejercicio-7.js
+++ ejercicio-7.js
@@ -13,3 +13,12 @@ for (let c = 0; c<=10; c++){
     producto = num * c;
     document.write(`${num} x ${c} = ${num*c} <br>`);
 }
+
+// El Ejecicio Plus se debe realizar mas o menos de la siguiente forma
+
+for (var c= 1 ;c<=num ; c++){
+    document.write("<h1>" Tabla del "+c + </h1>");
+    for(var i=1; i<=10; i++){
+        document.write(i +" X" + c +" = " +(i*c)+ "<br/>" );
+    }
+}
